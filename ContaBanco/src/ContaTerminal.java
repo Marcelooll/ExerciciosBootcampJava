24021ad9672a77042229198e9798d6272b33e0d7
Marcelooll <marceloggamer27@gmail.com>
2025-03-17 14:13:07 -0300

    Resolução do Desafio: Simulando uma Conta Bancária Através do Terminal/Console

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ContaTerminal {
+class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Por favor, digite o número da Conta:");
